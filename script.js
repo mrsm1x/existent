bcdb5c45a4fa4f0594c55a37244da88757a80ab5
mrsm1x <alberttataru1@gmail.com>
2024-05-19 11:32:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function showPage() {
 }
 
 function generateRandomString(length) {
-    const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    const characters = 'abcdef0123456789';
     let result = '';
     for (let i = 0; i < length; i++) {
         result += characters.charAt(Math.floor(Math.random() * characters.length));
